06520d6d9007b778ee6cfacd256e8e6e92e5cb9f
Aliaxander Novikov <nerevarin11@gmail.com>
2020-10-11 23:53:00 +0300

    feat:task check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+    arr = str.split('');
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let n = 0; n < bracketsConfig.length; n++) {
+            if (stack[stack.length - 1] == bracketsConfig[n][0] && arr[i] == bracketsConfig[n][1]) stack.pop();
+            else if (arr[i] == bracketsConfig[n][0]) stack.push(arr[i]);
+        }
+    }
+    if (stack.length == 0) {
+      return true;
+    } return false;
 }
+
