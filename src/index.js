bf68562236f47cd41bb4ff6a710fc096b1ea7711
Aliaxander Novikov <nerevarin11@gmail.com>
2020-10-16 19:50:48 +0300

    fix:fix solution

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
   let stack = [];
-    arr = str.split('');
-
-    for (let i = 0; i < arr.length; i++) {
+      
+  if (str.length % 2 !== 0) {
+    return false
+  }
+    for (let i = 0; i < str.length; i++) {
         for (let n = 0; n < bracketsConfig.length; n++) {
-            if (stack[stack.length - 1] == bracketsConfig[n][0] && arr[i] == bracketsConfig[n][1]) stack.pop();
-            else if (arr[i] == bracketsConfig[n][0]) stack.push(arr[i]);
+            if (stack[stack.length - 1] == bracketsConfig[n][0] && str[i] == bracketsConfig[n][1]) {
+              stack.pop();
+            } else if (str[i] == bracketsConfig[n][0]) {
+              stack.push(str[i]);
+            }
         }
-    }
-    if (stack.length == 0) {
+    } if (stack.length == 0) {
       return true;
     } return false;
 }
